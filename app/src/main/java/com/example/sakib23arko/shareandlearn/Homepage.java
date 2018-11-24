bf4c755e003551232dfceb29a19eb293ae8dac40
defendet01 <r.r.rahman.r.r@gmail.com>
2018-11-24 16:52:48 +0600

    first

--- app/src/main/java/com/example/sakib23arko/shareandlearn/Homepage.java
+++ app/src/main/java/com/example/sakib23arko/shareandlearn/Homepage.java
@@ -14,6 +14,7 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.firebase.FirebaseApp;
+import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -24,12 +25,15 @@ import java.util.ArrayList;
 
 public class Homepage extends AppCompatActivity {
 
-    private Button button,myProfileButton;
-    ArrayList<infoOfUser> userlist ;
+    private Button button, myProfileButton;
+    ArrayList<infoOfUser> userlist;
     ListView ListUserView;
     Bundle bundle;
     TextView continueReading;
     DatabaseReference userDatabase;
+    private Button signoutButton;
+    private FirebaseAuth mAuth;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,12 +44,25 @@ public class Homepage extends AppCompatActivity {
 
         myProfileButton = findViewById(R.id.myProfileButtonId);
         button = findViewById(R.id.CreateNewPostID);
+        signoutButton = findViewById(R.id.signoutButtonID);
         userlist = new ArrayList<>();
         ListUserView = findViewById(R.id.ListUserViewID);
         continueReading = findViewById(R.id.continueReadingid);
 
+        mAuth=FirebaseAuth.getInstance();
 
-        ListUserView.setOnItemClickListener( new AdapterView.OnItemClickListener() {
+        signoutButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mAuth.signOut();
+                Toast.makeText(Homepage.this,"You have signout",Toast.LENGTH_LONG).show();
+                finish();
+                startActivity(new Intent(Homepage.this,MainActivity.class));
+            }
+        });
+
+
+        ListUserView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> arg0, View arg1, int position, long id) {
                 Toast.makeText(Homepage.this, "sdakdas", Toast.LENGTH_SHORT).show();
@@ -64,14 +81,14 @@ public class Homepage extends AppCompatActivity {
         myProfileButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startActivity(new Intent(Homepage.this,userProfile.class));
+                startActivity(new Intent(Homepage.this, userProfile.class));
             }
         });
 
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(Homepage.this,CreateNewPost.class);
+                Intent intent = new Intent(Homepage.this, CreateNewPost.class);
                 startActivity(intent);
             }
         });
@@ -84,7 +101,7 @@ public class Homepage extends AppCompatActivity {
             @Override
             public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
                 userlist.clear();
-                for(DataSnapshot X : dataSnapshot.getChildren()) {
+                for (DataSnapshot X : dataSnapshot.getChildren()) {
                     userlist.add(0, X.getValue(infoOfUser.class));
                 }
                 UserList adapter = new UserList(Homepage.this, userlist);
